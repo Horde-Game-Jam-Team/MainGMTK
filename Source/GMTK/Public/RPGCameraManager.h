0a4301096a62553651b7201ed897b02cbe57edc5
Noe SELLAM <noe.sellam@epitech.eu>
2025-07-30 07:04:08 +0200

    Added empty c++ files

---
+++ Source/GMTK/Public/RPGCameraManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "RPGCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GMTK_API ARPGCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+};
